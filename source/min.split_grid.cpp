65ca6ecf1bb3c83dc503e49ca97c7454be8ee7cb
Ava Hazel <ava@ava>
2023-02-01 01:44:24 -0600

    created split_grid external for monome grid

---
+++ source/min.split_grid.cpp
@@ -0,0 +1,62 @@
+/// @file
+///	@ingroup 	minexamples
+///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
+///	@license	Use of this source code is governed by the MIT License found in the License.md file.
+
+#include "c74_min.h"
+
+using namespace c74::min;
+
+class split_grid : public object<split_grid> {
+
+public:
+    MIN_DESCRIPTION	{"Post to the Max Console."};
+    MIN_TAGS		{"utilities"};
+    MIN_AUTHOR		{"Cycling '74"};
+    MIN_RELATED		{"print, jit.print, dict.print"};
+
+    inlet<>  input	{ this, "List (int x, int y, int z)" };
+    outlet<> left	{ this, "Left half of grid" };
+	outlet<> right {this, "Right half of grid"};
+	outlet<> top {this, "Top half of grid"};
+	outlet<> bottom {this, "Bottom half of grid"};
+	outlet<> all {this, "Pass thru"};
+
+    // Process Note values
+	message<> list {this, "list",
+		MIN_FUNCTION {
+
+			// Error handling - list size
+			if (args.size() != 3) { 
+				cout << "Invalid Message - format is List (int x, int y, int z)" << endl;
+				return {};
+			}
+			
+			int x = args[0];
+			int y = args[1];
+			int z = args[2];
+			// Split left and right
+			if (x < 8) {
+				left.send(x, y, z);
+			}
+			else { 
+				right.send(x, y, z);
+			}
+			// Split top and bottom
+			if (y < 8) {
+				top.send(x, y, z);
+			}
+			else { 
+				bottom.send(x, y, z);
+			}
+
+			// Send all
+			all.send(x, y, z);
+
+			return {};
+		}
+	};
+};
+
+
+MIN_EXTERNAL(split_grid);
