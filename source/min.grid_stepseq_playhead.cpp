acbaccd05fe5889586938e23e1474f79885772f4
Ava Hazel <ava@ava>
2023-02-11 02:21:16 -0600

    Updated grid_stepseq_playhead

--- source/min.grid_stepseq_playhead.cpp
+++ source/min.grid_stepseq_playhead.cpp
@@ -11,20 +11,14 @@ class grid_stepseq_playhead : public object<grid_stepseq_playhead> {
 
 private:
 	int position;
-	int maxPosition;
-	int row [16];
 	bool on;
 
 	void sendOut() {
-		// send left half
-		output.send("setLevelRow", 
-			0, 0, row[0], row[1], row[2], 
-			row[3], row[4], row[5], row[6], row[7]);
+		// refresh LEDs before sending playhead
+		gridOutput.send("getLevelRow", 0);
 
-		// send right half
-		output.send("setLevelRow", 
-			8, 0, row[8], row[9], row[10], 
-			row[11], row[12], row[13], row[14], row[15]);
+		// send playhead
+		ledOutput.send("setLevelLED", position, 0, 15);
 	};
 
 public:
@@ -33,18 +27,14 @@ public:
     MIN_AUTHOR		{"Cycling '74"};
     MIN_RELATED		{"print, jit.print, dict.print"};
 
-    inlet<>  input	{ this, 
-		"number (0 = off, non-zero = on), getPosition, setPosition(int), getRow, setRow(int[16]), incrementPosition, decrementPosition" };
-    outlet<> output	{ this, "setLevelRow message (min.grid_led)" };
+    inlet<>  input	{ this, "number (0 = off, non-zero = on), setPosition(int)" };
+    outlet<> ledOutput	{ this, "setLevelRow message (min.grid_led)" };
+	outlet<> gridOutput {this, "getLevelRow message (min.grid_memory)"};
 
 
 	// Constructor
 	grid_stepseq_playhead(const atoms& args = {}) {
 		position = 0;
-		maxPosition = 15;
-		for (int i = 0; i <= maxPosition; i++) { 
-			row[i] = 0;
-		}
 		on = false;
 	}
 
@@ -60,104 +50,20 @@ public:
 		}
 	};
 
-
-	// Get the current position
-	message <> getPosition {this, "getPosition", 
-		MIN_FUNCTION {
-			output.send(position);
-			return {};
-		}
-	};
-
-	// Get the current position
+	// Set the current position
 	message <> setPosition {this, "setPosition", 
 		MIN_FUNCTION {
 			// Error handling
 			if (args.size() < 1) {
 				cout << "Invalid message: setPosition requires <args> (int index)";
 			}
-			if(position > maxPosition){
-				cout << "Invalid message: index out of bounds - max value is 15";
-			}
 			if (!on) { 
 				return {};
 			}
-			//Set previous position off
-			row[position] = 0;
 			//update position using incoming arg
 			position = args[0];
-			//Set new position on
-			row[position] = 15;
 
 			sendOut();
-			return {};
-		}
-	};
-
-	// Get the row
-	message<> getRow {this, "getRow",
-		MIN_FUNCTION {
-			// send out the row
-			sendOut();
-			return {};
-		}
-	};
-
-	// Set the row
-	message <> setRow {this, "setRow", 
-		MIN_FUNCTION {
-			// Error handling
-			if (args.size() < maxPosition + 1) {
-				cout << "Invalid message: setPosition requires <args> int[16]";
-			}
-			for(int i = 0; i < 16; i++){
-				//turn cells off
-				row[i] = 0;
-			}
-            //turn target cell on
-            row[position] = 15;
-			return {};
-		}
-	};
-
-	// Move the playhead
-	message <> incrementPosition {this, "incrementPosition", 
-		MIN_FUNCTION {
-
-			if (!on) { 
-				return {};
-			}
-			//Set previous position off
-			row[position] = 0;
-			//Increment position
-			if(position == maxPosition){
-				position = 0;
-			} else {
-				position++;
-			}
-			//Set new position on
-			row[position] = 15;
-			return {};
-		}
-	};
-
-	message<> decrementPosition {this, "decrementPosition", 
-		MIN_FUNCTION {
-			if (!on) { 
-				return {};
-			}
-			
-			// Set previous position off
-			row[position] = 0;
-			// Decrement position
-			if (position == 0) {
-				position = maxPosition;
-			}
-			else {
-				position--;
-			}
-			// Set new position on
-			row[position] = 15;
 			return {};
 		}
 	};
